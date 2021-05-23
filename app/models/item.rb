d86d345d4e7f32b536d39c34f1f5315acb54d9c0
emiumiu1 <emiumiu1@gmail.com>
2021-05-23 22:19:20 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+    validates :user, foreign_key: true
+  end
 end
